5435b40493edefe6eb137c16844bab2dc9443895
Tomoloju Victor <tomoloju45@gmail.com>
2019-02-13 22:32:08 -1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,37 +6,26 @@
 using namespace std;
 
 
-
 int main(){
 
-char vic[]
-= "+CWLAP:(0,\"CSIS_MH\",-86,\"1c:b9:c4:38:3a:b8\",1,41,0)+CWLAP:(0,\"CSIS_MH\",-73,\"1c:b9:c4:38:40:a8\",6,38,0)+CWLAP:(3,\"stop tryna be God\",-62,\"02:db:df:e4:f5:9d\",6,32767,0)+CWLAP:(3,\"island-17E290\",-51,\"30:87:d9:57:e2:93\",11,35,0)";
+char vic[500]
+= "+CWLAP:(0,\"CSIS_MH\",-86,\"1c:b9:c4:38:3a:b8\",1,41,0)\n+CWLAP:(0,\"CSIS_MH\",-73,\"1c:b9:c4:38:40:a8\",6,38,0)\n+CWLAP:(3,\"stop tryna be God\",-62,\"02:db:df:e4:f5:9d\",6,32767,0)\n+CWLAP:(3,\"island-17E290\",-51,\"30:87:d9:57:e2:93\",11,35,0)\n    OK";
 
+char net[7];
 
-
+cout<<strlen(vic)<<endl;
 get_creds(vic,"+CWLAP:(" , 4);
+
+cout<<strlen(vic)<<endl;
 cout<<vic<<endl;
-cout<<endl;
 
 
-cout<<"Please Select Your Connection"<<endl;
-cout<<endl;
-short a = 0;
 
-cin>>a;
+extract(vic , 4 , net);
 
-if(a >= 1 && a<=4){
-extract(vic,a);
-
+cout<<strlen(vic)<<endl;
 cout<<vic<<endl;
-cout<<endl;
-}
 
-else{
-    cout<<"INVALID"<<endl;
-cout<<endl;
-}
-
-
+cout<<net<<endl;
 
 }
